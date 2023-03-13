966076182c9c1410c206efa6d275aff039e81aa5
petyadamyanova <72454123+petyadamyanova@users.noreply.github.com>
2023-03-13 14:10:04 +0200

    new version

--- slave.ino
+++ slave.ino
@@ -1,10 +1,5 @@
 #include <SoftwareSerial.h>
 
-char bufferRx[100];
-char bufferTx[100];
-int nextfree=0;
-int id = 0;
-
 #define rxPinFrom 5
 #define txPinFrom 4
 #define rxPinTo 2
@@ -14,7 +9,152 @@ int id = 0;
 SoftwareSerial mySerialTo =  SoftwareSerial(rxPinTo, txPinTo);
 SoftwareSerial mySerialFrom = SoftwareSerial(rxPinFrom, txPinFrom);
 
+//================================================
+#define MAX_PACKET_SIZE 100
+#define PACKET_HEADER   0x23
+
+#define CMD_COUNT       0x43   // C
+
+// == All variables related to communication
+char bufferTx[MAX_PACKET_SIZE];
+char bufferRx[MAX_PACKET_SIZE];
+int nextFree;
+char dataToReceive;
+
+char tmpRx;
+
+//=================================================
+void clearTxBuffer() {
+  for(int i = 0 ; i < MAX_PACKET_SIZE; i ++) {
+    bufferTx[i] = 0;
+  }
+  nextFree=0;
+  Serial.println("Tx buffer cleared");
+}
+
+void clearRxBuffer() {
+  for(int i = 0 ; i < MAX_PACKET_SIZE; i ++) {
+    bufferRx[i] = 0;
+  }
+  nextFree=0;
+  Serial.println("Rx buffer cleared");
+}
+// ================================================
+// == System flags and variables
+char DeviceId = 0;
+// ================================================
+void generateCountPacket() {
+  generatePacket(CMD_COUNT, '1', &DeviceId);
+
+}
+void generatePacket(char cmd, char len, char *data) {
+  clearTxBuffer();
+
+  // Here we build our packet
+  bufferTx[nextFree++] = PACKET_HEADER;
+  bufferTx[nextFree++] = cmd;
+  bufferTx[nextFree++] = len;
+  
+  for(int i = 0; i < len - '0'; i++) {
+    bufferTx[nextFree++] = data[i];
+  }
+
+  bufferTx[nextFree++] = '\n';
+
+  for(int i = 0; i < nextFree; i++){
+    Serial.print(i);
+     Serial.print(" : ");
+    Serial.print(bufferTx[i],HEX);
+     Serial.println("");
+    mySerialTo.write(bufferTx[i]);
+  }
+  Serial.println("");
+}
+
+int receivePacket(){
+  int pos = 0; // possition to start reading
+
+  // regenerate packet
+  char packet_header_ = bufferRx[pos++];
+  char cmd_ = bufferRx[pos++];
+  char len_ = bufferRx[pos++];
+  char data_[len_];
+
+  for(int i=0;i<len_ - '0';i++){
+    data_[i] = bufferRx[pos++];
+  }
+
+  // testing
+  Serial.println("Start testing: ");
+  Serial.println(packet_header_, HEX);
+  Serial.println(cmd_, HEX);
+  Serial.println(len_, HEX);
+
+  for(int i=0;i<len_ - '0';i++){
+    Serial.println(data_[i], HEX);
+  }
+
+  Serial.println(bufferTx[pos++], HEX);
+
+  //Packet analysis
+
+  if(packet_header_ != PACKET_HEADER){
+    Serial.println("Bad header");
+    return -1;
+  }
+
+  switch(cmd_){
+    case CMD_COUNT:
+      DeviceId = data_[0] + 1;
+      Serial.println("New device Id is: ");
+      Serial.println(DeviceId); 
+      generateCountPacket();          
+      break;    
+    
+  }  
+  
+  clearRxBuffer();
+}
+
 void setup(){ 
+  clearTxBuffer();
+  mySerialTo.begin(9600);
+  mySerialFrom.begin(9600);
+  Serial.begin(9600);
+
+  //generateCountPacket();
+  
+
+  Serial.println("End init");
+}
+
+void loop() {
+  if(mySerialFrom.available()){
+    tmpRx = mySerialFrom.read();
+
+    if(nextFree == 0 && tmpRx == PACKET_HEADER) {
+      clearRxBuffer();
+    }
+
+    if(nextFree == 2) {
+      dataToReceive = tmpRx-'0';  // store how many bytes we have to receive - data len   
+    }
+
+    bufferRx[nextFree++] = tmpRx;  
+
+    if(dataToReceive == (nextFree - 3)){
+       receivePacket();
+    }    
+
+    Serial.write(bufferRx[nextFree-1]);
+  }
+
+  if(Serial.available()){
+    mySerialTo.write(Serial.read());
+  }
+}
+
+/*void setup(){ 
   mySerialTo.begin(9600);
   mySerialFrom.begin(9600);
   Serial.begin(9600);
@@ -29,8 +169,6 @@ void loop() {
       nextfree=0;
     }
 
-    
-    
   }
 
   if(mySerialFrom.available()){
@@ -44,4 +182,4 @@ void loop() {
   }
 
  
-}
+}*/
